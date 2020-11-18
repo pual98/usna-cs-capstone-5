d32dfeab47ee254f1bc00cfa64561e30a8f172fe
pual98 <paul.slife@protonmail.com>
2020-11-18 16:23:03 -0500

    Merging master

---
+++ code/Dataset.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+
+public class Dataset
+{
+  public ArrayList<Entity> build(String filename)
+  {
+    BufferedReader source;
+    ArrayList<Entity> ret = new ArrayList<Entity>();
+    try{
+      source = new BufferedReader(new FileReader(filename));
+      String line = "";
+      int i = 0;
+      while(source.ready())
+	{
+	  line = source.readLine();
+	  Entity en = Entity.makeEntityFromString(line);
+	  if(en != null)
+	    {
+	      ret.add(en);
+	    }
+	}
+    }
+    catch(Exception e)
+      {
+	e.printStackTrace();
+	System.exit(1);
+      }
+    return ret;
+  }
+}
