374cddb4d8fff1240f8b14846ec43c24bc37a39a
Pattybish23 <m210480@usna.edu>
2021-03-18 21:23:25 -0400

    made randomization of clusters a little less random

--- tSNEplot.py
+++ tSNEplot.py
@@ -3,13 +3,13 @@ import numpy as np
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("output1.csv")
+df = pd.read_csv("output4.csv")
 df.columns = ['ip src', 'ip dest', 'timestamp', 'gen type', 'port src', 'port dest', 'msg type', 'label']
 
-df2 = pd.read_csv("output2.csv")
+df2 = pd.read_csv("output5.csv")
 df2.columns = ['ip src', 'ip dest', 'timestamp', 'gen type', 'port src', 'port dest', 'msg type', 'label']
 
-df3 = pd.read_csv("output3.csv")
+df3 = pd.read_csv("output6.csv")
 df3.columns = ['ip src', 'ip dest', 'timestamp', 'gen type', 'port src', 'port dest', 'msg type', 'label']
 
 df = df.append(df2)
